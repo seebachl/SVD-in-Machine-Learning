ff974aade59431cacff5889e9144dcbd077615bf
seebachl <114714427+seebachl@users.noreply.github.com>
2023-03-21 20:08:05 -0400

    Update SVD.m

--- SVD.m
+++ SVD.m
@@ -10,54 +10,79 @@ A1 = rand(m,n)
 % find SVD decomposition of A
 [U,Sigma,V] = svd(A1)
 %% Find low-rank approximation of A using SVD
-% create random matrix of size m x n and find its SVD
-m = 5;
-n = 3;
-A = rand(m,n)
-[U_k,Sigma,V_k] = svd(A);
+% create random matrix of size 30 x 30 with a given spectrum and find its SVD
+evals = [100000 9000 5000 2000 1500 900 700 300 200 121 25 10 5 2 1 0.945 ...
+    0.9 0.877 0.843 0.8 0.7 0.75 0.4 0.3 0.002 0.0032 0.004 0.0002 0.0001 ...
+    0.0001]; %d efine the spectrum
+dec_evals = sort(evals,'descend'); % sort the eigenvalues from large to small
+
+fig1 = figure; clf;
+plot(dec_evals,'bo-') % plot the eigenvalues
+xlabel('Singular Value Position')
+
+Adiag = diag(dec_evals,0); % create a diagonal matrix with eigenvalue entries
+T = rand(30); % create a random 30 x 30 matrix
+A = T * Adiag * inv(T); % perform a similarity transformation
+[U_k,Sigma,V_k] = svd(A); % find SVD 
 
 % decide how many singular values to keep ( <n)
 % change this value to see how the error differs
-ksigma = 3;
+ksigma = 1;
 
 % find the best rank k approximation of A by using a linear combination of
 % outerproducts with the singular value of A as the weights
 A_best = zeros(size(A));
 for i = 1:ksigma
-    A_best = A_best + (Sigma(i) * U_k(:,i) * V_k(:,i)');
+    A_best = A_best + (Sigma(i,i) * U_k(:,i) * V_k(:,i)');
 end
 
-A_best
+A_best;
 
 % find the error of the best rank k approximation of A
-err_A_best = norm((A-A_best),"fro")
+err = norm(A-A_best)
+err_A_best = norm(err)/norm(A)
+
+% plot error as a function of the rank k approximation of A 
+A_best = zeros(size(A));
+err_list = zeros(30,1)
+for k = 1:30
+    A_best = zeros(size(A));
+    for i = 1:k
+        A_best = A_best + (Sigma(i,i) * U_k(:,i) * V_k(:,i)');
+    end
+    err_list(i) = norm(A-A_best)/norm(A)
+end
+
+fig2 = figure; clf;
+plot(err_list,'bo-')
+xlabel('Number of Singular Values Used') 
+ylabel('Relative Error of Resulting Approximation Matrix') 
 
 %% Find the pseudo-inverse of A
 % create random matrix A
-m = 5;
-n = 4;
-A = rand(m,n);
+A = [-1 1 0; -1 0 1; 0 -1 1]
+b = [900; -601; 10]
+
+A\b % note this gives no solution since A is non-singular
 
 % built in pesudeo-inverse command "pinv", works for any case
 pinvA = pinv(A)
+pinvA*b %solves issue with least squares solution
 
-% to compute manually, need to divide into cases
-% rank deficient case
-A_rd = [-1 1 0; -1 0 1; 0 -1 1];
-[m,n] = size(A_rd)
-[U,S,V] = svd(A_rd)
-U1 = U(:,1:n)
-S1 = S(1:n,:)
-invS1 = zeros(size(S1))
-for i = 1:n
-    if S1(i,i) ~= 0
-        invS1(i,i) = 1/S1(i,i)
-    else 
-        invS1(i,i) = 0
+%% Principal Component Analysis
+m=5;
+n=4;
+A = rand(m,n);
+centered_A = zeros(size(A));
+for i = 1:m
+    for j = 1:n
+        centered_A(i,j) = A(i,j)-mean(A(:,j))
     end
 end
-pinvA = V*invS1*U1'
-pinvA1 = pinv(A_rd)
+
+coeff = pca(A)
 
 
+[U,S,V] =svd(centered_A)
 
+norm(V-coeff,2)
