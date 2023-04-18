95aaffc3ce2c74c8e72a40d38fadecb8a51b0a9b
seebachl <114714427+seebachl@users.noreply.github.com>
2023-04-18 12:50:02 -0400

    Update Facial_Recognition.m

--- Facial_Recognition.m
+++ Facial_Recognition.m
@@ -1,5 +1,10 @@
-%% Compressing multiple images in a database into one matrix 
+%% An example of a simple facial recognition algorithm using PCA and eigenfaces 
+% Compress multiple images in a database into one matrix
+% ensure all documents from Celebrity-Images and Test-Images are downloaded
+% on your device
+clc
 clear
+
 F = zeros(100*100, 17);
 for k = 1:17
   Filename = strcat('face', num2str(k), '.jpg');
@@ -8,13 +13,14 @@ for k = 1:17
   F(:,k) = grayscale(:);
 end
 
-%Average Face
+% Finding the average Face
 mu = mean(F,2);
 F = F - repmat(mu, 1, 17);
 imshow(mat2gray(reshape(mu, [100, 100]))) %this is the average face
 title("Average Face")
 
-%% Finding the Eigenfaces 
+%% Finding the eigenfaces of the matrix F using the method described in the paper
+% computing the true covariance matrix takes a very long time 
 
 C = F'*F; 
 [evec, eval] = eig(C); %find the eigenvectors and values of F'F
@@ -23,10 +29,10 @@ for i = 1:size(evec,2)
     u = F*evec(:,i)/(norm(F*evec(:,i))); %multiply by F and normalize to find orthonormal basis for range of F (face space)
     U(:,i) = u;
 end
-[~, ind] = sort(diag(eval), 'descend'); % order eigenfaces based on corresponding eigenvalues
+[eval, ind] = sort(diag(eval), 'descend'); % order eigenfaces based on corresponding eigenvalues
 U = U(:,ind);
 
-% Plot Eigenfaces
+% Plot and show the first 8 eigenfaces
 for i = 1:8 
     img = reshape(U(:,i), [100,100]);
     subplot(2,4,i);
@@ -35,8 +41,8 @@ for i = 1:8
 end
 %% Example of reconstructing one face in lower dimension
 % Code adapted from Machine Learning : A Bayesian and Optimization Perspective by Sergios Theodoridis
-% Chose which image to recreate (change second index) 
-X = F(:,1);
+% Chose which image to recreate (change second index to choose a different face) 
+X = F(:,2);
 
 % Project image onto face space (find weight vector)
 Y = U'*X;
@@ -45,27 +51,27 @@ Y = U'*X;
 [~,indx] = sort(abs(Y), 'descend');
 
 % Recreate face using different amount of eigenfaces (change sigma)
-ksigma = 17
+% increasing ksigma will improve the image
+ksigma = 5
 R = U(:,indx(1:ksigma))*Y(indx(1:ksigma))+mu; 
 imshow(mat2gray(reshape(R, 100, 100)))
 
-%% Facial Recognition
+%% Facial recognition -- using an image from the Test-Images file, this code identifies the face based on the Celebrity-Images data set
 % Read the image turn it into a vector
-imageData = imread('identifywinslet.jpg');
+imageData = imread('identifyjolie.jpg');
 grayscale = rgb2gray(imageData);
-unknown = grayscale(:)
+unknown = grayscale(:);
 imshow(mat2gray(grayscale))
 
 % Find face space projection of known images 
 % (Similar to Omega_l but there is only one face for each class)
 x = U'*F;
-size(x)
 
 % Project the image onto the face space
 omega = U'*(double(unknown) - mu); %finding weights of new image
 
-% find the Euclidean distance
-eps = zeros(17,1)
+% find the Euclidean distance from each weight vector
+eps = zeros(17,1);
 for i = 1:17
     eps(i) = norm(x(:,i)-omega);
 end
@@ -74,5 +80,8 @@ end
 % we use minimum instead of threshold since we know the face belongs to
 % someone in our set
 % the corresponding image (face(i).jpg) is who the system is the image recognizing as
-[~, i] = (min(eps))
-sprintf("This is the same person as face%i", i)
+[~, i] = (min(eps));
+data = [[1:17]; ["Angelina Jolie", "Brad Pitt", "Denzel Washington", "Hugh Jackman", "Jennifer Lawrence", "Johnny Depp", "Kate Winslet", "Leonardo DiCaprio", "Megan Fox", "Natalie Portman", "Nicole Kidman", "Robert Downey Jr.", "Sandra Bullock", "Scarlett Johansson", "Tom Cruise", "Tom Hanks", "Will Smith"]];
+sprintf("This is %s", data(2,i))
+
+
