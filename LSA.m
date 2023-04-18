df79a43e92e071b3819da30fe418e7ebcdcdf0c2
seebachl <114714427+seebachl@users.noreply.github.com>
2023-04-18 12:46:26 -0400

    Update LSA.m

--- LSA.m
+++ LSA.m
@@ -1,6 +1,7 @@
-%% An example of LSA using SVD
+%% An example of latent semantic analysis of textbook titles using SVD
 % First we set up the term-document matrix
-% we use titles of first and second year math textbooks at McMaster University for simplicity
+% we use titles of first- and second-year math textbooks at McMaster University for simplicity
+clc
 clear
 
 % Enter the titles 
@@ -20,7 +21,7 @@ d13 = ["introduction", "to", "probability"];
 d14 = ["probability", "and", "statistics", "for", "engineering", "and", "the", "sciences"];
 l = max([length(d1), length(d2), length(d3), length(d4), length(d5), length(d6), length(d7), length(d8), length(d9), length(d10), length(d11), length(d12), length(d13), length(14)]);
 
-% creating a matrix of the documents 
+% creating a matrix of the titles
 d = [[d1 zeros(1, l-length(d1))]' [d2 zeros(1, l-length(d2))]' [d3 zeros(1, l-length(d3))]' [d4 zeros(1, l-length(d4))]' [d5 zeros(1, l-length(d5))]' [d6 zeros(1, l-length(d6))]' [d7 zeros(1, l-length(d7))]' [d8 zeros(1, l-length(d8))]' [d9 zeros(1, l-length(d9))]' [d10 zeros(1, l-length(d10))]' [d11 zeros(1, l-length(d11))]' [d12 zeros(1, l-length(d12))]' [d13 zeros(1, l-length(d13))]' [d14 zeros(1, l-length(d14))]'];
 
 % replacing words three letters or less with "0" 
@@ -35,10 +36,10 @@ end
 % removing duplicate words and turning matrix into vector 
 terms = unique(d);
 terms_sort = terms(2:length(terms)); %these are the terms present in our documents, in alphabetical order, not including words less than three letters
-term_doc = zeros(length(terms_sort),size(d,2));
 
 % creating term-document matrix
-% for simplicity, we use term frequency local weighting and no global weighting
+% for simplicity, we use raw term-frequency local weighting and no global weighting 
+term_doc = zeros(length(terms_sort),size(d,2));
 for j = 1:size(d,2)
      for i = 1:length(terms_sort)    
          for k = 1:length(d(:,j))
@@ -50,11 +51,11 @@ for j = 1:size(d,2)
 end
 
 term_doc; % this is our term-document matrix
-%% Performing SVD on the term-document matrix 
+%% Performing SVD on the term-document matrix to find the best k-rank approximation
 [U, S, V] = svd(term_doc);
 
 % Finding the low rank approximation 
-ksigma = 5; % change this to change the dimension
+ksigma = 5; % change this value to change the dimension
 latent = zeros(size(term_doc));
 for i = 1:ksigma
     latent = latent + (S(i,i) * U(:,i) * V(:,i)'); % this is our approximation matrix 
@@ -66,19 +67,15 @@ labels2 = ["d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "
 fig1 = figure; clf;
 hold on
 
-% plot U for the terms 
-%plot(U(:,1)*S(1,1), U(:,2)*S(2,2), 'o')
-%text(U(:,1)*S(1,1), U(:,2)*S(2,2), labels1,'VerticalAlignment','bottom','HorizontalAlignment','right')
-
 %plot V for the documents
 scatter(V(:,1)*S(1,1), V(:,2)*S(2,2))
 text(V(:,1)*S(1,1), V(:,2)*S(2,2), labels2,'VerticalAlignment','bottom','HorizontalAlignment','right')
 grid on; axis([-3 1 -3 1])
 title('A Two-Dimensional Plot of Documents')
-
 %% Finding documents based on a query
 % enter a query vector
 query = ["statistics"];
+% query = ["mathematics" "applications"]; % try this query too!
 q = zeros(length(terms_sort),1);
 for k = 1:length(query)
     for i = 1:length(terms_sort)
@@ -88,13 +85,13 @@ for k = 1:length(query)
     end
 end
 
-qhat = q'*(U(:,1:ksigma))*(inv(S(1:ksigma,1:ksigma))); % find q's mapping to the semantic space
+qhat = q'*(U(:,1:ksigma))*(inv(S(1:ksigma,1:ksigma))); % find q's representation in the semantic space
 
 %compute cosine similarity
 cos_sim = zeros(length(l),1);
 for i = 1:14
         f1 = V(i,1:ksigma)*qhat';
-        cos_sim(i) = (f1/(norm(qhat)*norm(V(i,1:ksigma)')));
+        cos_sim(i) = f1/(norm(qhat')*norm(V(i,1:ksigma)));
 end
 
 % find the documents with a cosine similarity larger than 0.9
@@ -105,31 +102,29 @@ for i = 1:length(L)
     end
 end
 
+%% Creating a figure for the semantic vector space with the query (ksigma = 2) 
 % compute the query in 2-D
 q2d = q'*(U(:,1:2))*inv(S(1:2,1:2));
 
 labels3 = ["q"];
 hold on
-%plot(U(:,1)*S(1,1), U(:,2)*S(2,2), 'o')
-%text(U(:,1)*S(1,1), U(:,2)*S(2,2), labels1,'VerticalAlignment','bottom','HorizontalAlignment','right')
-% plot(V(:,1)*S(1,1), V(:,2)*S(2,2), 'o')
-% text(V(:,1)*S(1,1), V(:,2)*S(2,2), labels2,'VerticalAlignment','bottom','HorizontalAlignment','right')
 
-scatter(V(:,1)*S(1,1), V(:,2)*S(2,2))
-text(V(:,1)*S(1,1), V(:,2)*S(2,2), labels2,'VerticalAlignment','bottom','HorizontalAlignment','right')
-plot(q2d(1), q2d(2), 'o')
-text(q2d(1), q2d(2), labels3,'VerticalAlignment','top','HorizontalAlignment','right')
+% plot query with documents 
+scatter(V(:,1)*S(1,1), V(:,2)*S(2,2), "filled")
+text(V(:,1)*S(1,1), V(:,2)*S(2,2), labels2,'VerticalAlignment','bottom','HorizontalAlignment','right','FontSize', 14)
+scatter(q2d(1), q2d(2), "filled")
+text(q2d(1), q2d(2), labels3,'VerticalAlignment','top','HorizontalAlignment','right', 'FontSize', 14)
 grid on; axis([-3 1 -3 1])
 title('A Two-Dimensional Plot of Documents with Query')
 
 %% Updating
 % we add select third year textbooks
-d18 = ["introduction", "to", "mathematical", "statistics"];
-d19 = ["applied", "statistics", "and", "probability", "for", "engineering"];
+d15 = ["introduction", "to", "mathematical", "statistics"];
+d16 = ["applied", "statistics", "and", "probability", "for", "engineering"];
 
 % creating a matrix of the documents 
-l1 = max([length(d18), length(d19)]);
-d1 = [[d18 zeros(1, l-length(d18))]' [d19 zeros(1, l-length(d19))]'];
+l1 = max([length(d15), length(d16)]);
+d1 = [[d15 zeros(1, l-length(d15))]' [d16 zeros(1, l-length(d16))]'];
 
 % replacing words three letters or less with "0" 
 for i = 1:size(d1,1)
@@ -157,3 +152,20 @@ Vkp = V(:,1:ksigma)';
 for i = 1:size(newterm_doc,2)
     Vkp = [Vkp [newterm_doc(:,i)'*(U(:,1:ksigma))*(inv(S(1:ksigma,1:ksigma)))]'];
 end
+
+mapd15 = newterm_doc(:,1)'*(U(:,1:2))*(inv(S(1:2,1:2)));
+mapd16 = newterm_doc(:,2)'*(U(:,1:2))*(inv(S(1:2,1:2)));
+% plot new documents with old
+% notice placement of old documents don't change
+figure
+hold on
+labels4 = ["d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "d12", "d13", "d14", "d15", "d16"];
+scatter(V(:,1)*S(1,1), V(:,2)*S(2,2), "filled")
+text(V(:,1)*S(1,1), V(:,2)*S(2,2),labels2,'HorizontalAlignment','right' ,'FontSize', 14)
+scatter(mapd15(1)*S(1,1), mapd15(2)*S(2,2), "filled")
+text(mapd15(1)*S(1,1), mapd15(2)*S(2,2), "d15",'VerticalAlignment','top','HorizontalAlignment','right' ,'FontSize', 14)
+scatter(mapd16(1)*S(1,1), mapd16(2)*S(2,2), "filled")
+text(mapd16(1)*S(1,1), mapd16(2)*S(2,2), "d16",'VerticalAlignment','top','HorizontalAlignment','right', 'FontSize', 14 )
+title('A Two-Dimensional Plot of Documents after Folding-in')
+
+grid on; axis([-3 1 -3 1])
